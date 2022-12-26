d0c25e057c1da8ddab618e6c5cb1b919fc90b1de
Dmitry <burmitr@yandex.ru>
2022-12-26 20:05:34 +0500

    dz5

---
+++ Urok 5. Uskorennaya obrabotka dannih/dz5.py
@@ -0,0 +1,37 @@
+# Игра морской бой
+import random
+
+game_field = [['+ ', '+ ', '+ '], ['+ ', '+ ', '+ '], ['+ ', '+ ', '+ ']]
+field_size = 3
+
+x_of_ship = random.randint(1, field_size)
+y_of_ship = random.randint(1, field_size)
+
+x_of_strike = 0
+y_of_strike = 0
+
+def print_game_field():
+    print(" ", end=" ")
+    for k in range(field_size):
+        print(f'{k+1}', end='  ')
+    print()
+    for i in range(field_size):
+        print(f'{i+1}', end=' ')
+        for j in range(field_size):
+            print(game_field[i][j], end=' ')
+        print()
+
+print_game_field()
+
+while x_of_ship != x_of_strike and y_of_ship != y_of_strike:
+    x_of_strike = int(input(f'Для выстрела введите номер строки от 1 до {field_size}: '))
+    y_of_strike = int(input(f'а теперь выберите номер столбца от 1 до {field_size}: '))
+    if x_of_ship != x_of_strike and y_of_ship != y_of_strike:
+        game_field[x_of_strike - 1][y_of_strike - 1] = '0 '
+        print_game_field()
+        print("Мазила! ;-Р Пробуй ещё!")
+        print()
+    else:
+        game_field[x_of_strike - 1][y_of_strike - 1] = 'Х '
+        print_game_field()
+        print('Завалил кабанчика! :)')
--- Urok 5. Uskorennaya obrabotka dannih/sem5.py
+++ Urok 5. Uskorennaya obrabotka dannih/sem5.py
@@ -10,3 +10,24 @@
 # nums = [x**3 for x in range(n) if x % 2 == 0]
 # print(nums)
 
+
+# Задача написать программу игры "Камень - ножницы - бумага"
+
+# from random import randint
+# list1 = {1: "Камень", 2:"Ножницы", 3:"Бумага"}
+#
+# while True:
+#     player = int(input('1 - Камень, 2 - Ножницы, 3 - Бумага. Выберите вариант хода: '))
+#     if player == 0:
+#         break
+#     else:
+#         comp_choice = randint(1, 3)
+#         print(comp_choice)
+#         if player == comp_choice:
+#             print('Draw!')
+#         elif player == 1 and comp_choice == 2 or player == 3 and comp_choice == 1 or player == 2 and comp_choice == 3:
+#             print ('You win!!!')
+#         else:
+#             print('Вы проиграли!((((' )
+
+
